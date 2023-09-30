c2877755dcfaa6157e96149d0954c9962c080102
Jhonattas Ferreira <jfs120@gmail.com>
2023-09-30 00:55:52 -0300

    increase text

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 const Home = () => {
-    return <h1>{`Toma Mariazinha, é pra você esses coraçãozinhos ---> 💖💖💖`}</h1>
+    return (
+        <>
+            <h1>{`Toma Mariazinha, é pra você esses coraçãozinhos ---> 💖💖💖`}</h1>
+            <h1>{`Se gostou, manda um beijinho ---> 😘😘😘`}</h1>
+        </>
+    )
 }
 
 export default Home;
