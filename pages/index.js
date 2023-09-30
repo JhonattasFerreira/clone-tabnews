f890d3f1ae529cf423413bc8786496c007bcb8c0
Jhonattas Ferreira <jfs120@gmail.com>
2023-09-30 00:35:07 -0300

    modify text

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 const Home = () => {
-    return <h1>{`Toma mariazinha, é pra você esse coraçãozinho ---> 💖`}</h1>
+    return <h1>{`Toma Mariazinha, é pra você esse coraçãozinho ---> 💖`}</h1>
 }
 
 export default Home;
