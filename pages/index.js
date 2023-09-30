9ac419e1397d9df8aa777c52403ac5c55b111f42
Jhonattas Ferreira <jfs120@gmail.com>
2023-09-30 01:10:00 -0300

    add video

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 const Home = () => {
     return (
         <>
-            <h1>{`Toma Mariazinha, é pra você esses coraçãozinhos ---> 💖💖💖`}</h1>
-            <h1>{`Se gostou, manda um beijinho ---> 😘😘😘`}</h1>
+            <h1 align="center">{`Toma Mariazinha, é pra você esses coraçãozinhos ---> 💖💖💖`}</h1>
+            <h1 align="center">{`Se gostou, manda um beijinho ---> 😘😘😘`}</h1>
+            <div align="center">
+                <iframe width="380" height="315" src="https://www.youtube.com/embed/Tcx6YyXvvRI" title="Penguin Fail - Best Bloopers from Penguins Spy in the Huddle (Waddle all the Way)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
         </>
     )
 }
