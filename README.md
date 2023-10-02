a48d3d766d6ff9a62e6b8e213a56266df4b3a5f3
Jhonattas Ferreira <jfs120@gmail.com>
2023-10-02 19:56:04 -0300

    add `lint:check` and `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Just a clone of tabnews site
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
 const Home = () => {
-    return (
-        <>
-            <h1 align="center">{`Toma Mariazinha, é pra você esses coraçãozinhos ---> 💖💖💖`}</h1>
-            <h1 align="center">{`Se gostou, manda um beijinho ---> 😘😘😘`}</h1>
-            <div align="center">
-                <iframe width="380" height="315" src="https://www.youtube.com/embed/Tcx6YyXvvRI" title="Penguin Fail - Best Bloopers from Penguins Spy in the Huddle (Waddle all the Way)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <h1 align="center">{`Toma Mariazinha, é pra você esses coraçãozinhos ---> 💖💖💖`}</h1>
+
+      <h1 align="center">{`Se gostou, manda um beijinho ---> 😘😘😘`}</h1>
+
+      <div align="center">
+        <iframe
+          width="380"
+          height="315"
+          src="https://www.youtube.com/embed/Tcx6YyXvvRI"
+          title="Penguin Fail - Best Bloopers from Penguins Spy in the Huddle (Waddle all the Way)"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </>
+  );
+};
 
 export default Home;
